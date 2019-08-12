bbea138f20f0ae2a175065bda982995e19fb0308
Yeming Liu <Yeming.Liu@microsoft.com>
2019-08-12 17:14:55 +0800

    a utility class for arg completer

---
+++ src/ResourceManager.Test/ArgumentCompleterUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters;
+
+namespace Microsoft.Azure.Commands.ResourceManager.Tests
+{
+    public class ArgumentCompleterUnitTests
+    {
+        [Fact]
+        public void ReturnsCorrectScript()
+        {
+            var parameters = new[] { "Location", "Type" };
+            var libNamespace = "Microsoft.Azure.Commands.Management.Compute.ArgumentCompleters";
+            var className = "VmssSkuCompleterAttribute";
+            var methodName = "GetSkuNames";
+            string script = new ArgumentCompleterUtility.ScriptBuilder(parameters, libNamespace, className, methodName).ToString();
+            Assert.Equal(@"param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+$var0 = $fakeBoundParameter['Location']
+$var1 = $fakeBoundParameter['Type']
+$skuNames = [Microsoft.Azure.Commands.Management.Compute.ArgumentCompleters.VmssSkuCompleterAttribute]::GetSkuNames($var0, $var1)
+$locations | Where-Object { $_ -Like ""$wordToComplete*"" } | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }", script);
+        }
+    }
+}
---
+++ src/ResourceManager/Version2016_09_01/ArgumentCompleters/SkuCompleter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Text;
+
+namespace Microsoft.Azure.Commands.ResourceManager.Common.Version2016_09_01.ArgumentCompleters
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class SkuNameCompleterAttribute : ArgumentCompleterAttribute
+    {
+        public SkuNameCompleterAttribute() : base(CreateScriptBlock())
+        {
+        }
+
+        protected static ScriptBlock CreateScriptBlock()
+        {
+            string script = "param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)\n" +
+                "$skuNames = [Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters.SkuCompleterAttribute]::GetSkuNames()\n" +
+                "$skuNames | Where-Object { $_ -Like \"*$wordToComplete*\" } | ForEach-Object { [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_) }";
+            ScriptBlock scriptBlock = ScriptBlock.Create(script);
+            return scriptBlock;
+        }
+
+        public static string[] GetSkuNames()
+        {
+            return new string[] { };
+        }
+    }
+}
