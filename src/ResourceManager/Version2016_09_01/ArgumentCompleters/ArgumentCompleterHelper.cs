e303d739c7f1bf80ff6adcaac1901e5afdfcfd28
Yeming Liu <Yeming.Liu@microsoft.com>
2019-08-19 17:15:54 +0800

    docs

--- src/ResourceManager/Version2016_09_01/ArgumentCompleters/ArgumentCompleterHelper.cs
+++ src/ResourceManager/Version2016_09_01/ArgumentCompleters/ArgumentCompleterHelper.cs
@@ -20,8 +20,14 @@ namespace Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters
     using Microsoft.Azure.Commands.Common.Authentication.Abstractions;
     using Microsoft.Rest.Azure;
 
+    /// <summary>
+    /// Provides helper functions and classes that other argument completers can use.
+    /// </summary>
     public static class ArgumentCompleterHelper
     {
+        /// <summary>
+        /// Builds the actual ps script for argument completer.
+        /// </summary>
         public class ScriptBuilder
         {
             private readonly string[] requiredParameters;
@@ -29,6 +35,13 @@ namespace Microsoft.Azure.Commands.ResourceManager.Common.ArgumentCompleters
             private readonly string className;
             private readonly string methodName;
 
+            /// <summary>
+            /// Constructor. Call `ToString()` to get the result.
+            /// </summary>
+            /// <param name="requiredParameters">Specify the names of other parameters, whose values are going to be passed to `method` to sort out the completion result.</param>
+            /// <param name="libNamespace"></param>
+            /// <param name="className"></param>
+            /// <param name="methodName">The name of the method that is called to sort out the completion result.</param>
             public ScriptBuilder(string[] requiredParameters, string libNamespace, string className, string methodName)
             {
                 this.requiredParameters = requiredParameters;
@@ -49,6 +62,13 @@ $candidates | Where-Object {{ $_ -Like ""$wordToComplete*"" }} | Sort-Object | G
             }
         }
 
+        /// <summary>
+        /// Synchronously reads all paged data and concat them together.
+        /// </summary>
+        /// <param name="task">A task to get first page.</param>
+        /// <param name="nextTaskCreator">A function to get next page according to its link.</param>
+        /// <typeparam name="TItem"></typeparam>
+        /// <returns>The concatenated items list.</returns>
         public static List<TItem> ReadAllPages<TItem>(Task<IPage<TItem>> task, Func<string, Task<IPage<TItem>>> nextTaskCreator)
         {
             var results = new List<TItem>();
@@ -68,6 +88,11 @@ $candidates | Where-Object {{ $_ -Like ""$wordToComplete*"" }} | Sort-Object | G
             return results;
         }
 
+        /// <summary>
+        /// A common function to hash the context, so that the result can be used as a key for caching.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static int HashContext(IAzureContext context)
         {
             return (context.Account.Id + context.Environment.Name + context.Subscription.Id + context.Tenant.Id).GetHashCode();
