8ac8b37130301e4306645ddeb2a3314cd78ae8eb
Yabo Hu <yabhu@microsoft.com>
2020-10-15 10:08:26 +0800

    Update AzurePSCmdlet.cs

--- src/Common/AzurePSCmdlet.cs
+++ src/Common/AzurePSCmdlet.cs
@@ -49,7 +49,7 @@ namespace Microsoft.WindowsAzure.Commands.Utilities.Common
         private AzurePSDataCollectionProfile _cachedProfile = null;
 
         private IList<Regex> _matchers = new List<Regex>();
-        private static readonly Regex _defaultMatcher = new Regex("(\\s*\"refresh_token\"\\s*:\\s*)\"[^\"]+\"");
+        private static readonly Regex _defaultMatcher = new Regex("(\\s*\"access_token\"\\s*:\\s*)\"[^\"]+\"");
 
         protected AzurePSDataCollectionProfile _dataCollectionProfile
         {
