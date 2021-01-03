ac0d95a12a297cc4e745f9a6403bafcacd08af95
Erich(Renyong) Wang <eriwan@microsoft.com>
2021-01-04 00:20:41 +0800

    initial check in for Azure PowerShell related exceptions

---
+++ src/Authentication.Abstractions/Exceptions/AzPSArgumentNullException.cs
@@ -0,0 +1,101 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+using Microsoft.Azure.Commands.Common.Authentication.Abstractions.Interfaces;
+
+namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions.Exceptions
+{
+    public class AzPSArgumentException : ArgumentException, IContainsTelemetryErrorData
+    {
+        public ErrorKind ErrorKind
+        {
+            get
+            {
+                return Data.Contains(AzurePSTelemetryKeys.ErrorKindKey) ?
+                    Data[AzurePSTelemetryKeys.ErrorKindKey] as ErrorKind : null;
+            }
+
+            private set { Data[AzurePSTelemetryKeys.ErrorKindKey] = value; }
+        }
+
+        public string DesensitizedErrorMessage
+        {
+            get
+            {
+                return Data.Contains(AzurePSTelemetryKeys.DesensitizedErrorMessageKey) ?
+                    Data[AzurePSTelemetryKeys.DesensitizedErrorMessageKey]?.ToString() : null;
+            }
+
+            private set { Data[AzurePSTelemetryKeys.DesensitizedErrorMessageKey] = value; }
+        }
+
+        public int? ErrorLineNumber
+        {
+            get
+            {
+                return Data.Contains(AzurePSTelemetryKeys.ErrorLineNumberKey) ?
+                    (int?)Data[AzurePSTelemetryKeys.ErrorLineNumberKey] :
+                    null;
+            }
+
+            private set { Data[AzurePSTelemetryKeys.ErrorLineNumberKey] = value; }
+        }
+
+        public string ErrorFileName
+        {
+            get
+            {
+                return Data.Contains(AzurePSTelemetryKeys.ErrorFileNameKey) ?
+                    Data[AzurePSTelemetryKeys.ErrorFileNameKey]?.ToString() : null;
+            }
+
+            private set { Data[AzurePSTelemetryKeys.ErrorFileNameKey] = value; }
+        }
+
+        public AzPSArgumentException(
+            string message,
+            string paramName,
+            string desensitizedErrorMessage,
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerFilePath] string filePath = null)
+            : this(message, paramName, desensitizedErrorMessage, ErrorKind.UserError, lineNumber, filePath)
+        {
+        }
+
+        public AzPSArgumentException(
+            string message,
+            string paramName,
+            string desensitizedErrorMessage,
+            ErrorKind errorKind,
+            [CallerLineNumber] int lineNumber = 0,
+            [CallerFilePath] string filePath = null)
+            :base(paramName, message)
+        {
+            ErrorKind = errorKind;
+            if (!string.IsNullOrEmpty(desensitizedErrorMessage))
+            {
+                DesensitizedErrorMessage = desensitizedErrorMessage;
+            }
+            ErrorLineNumber = lineNumber;
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                ErrorFileName = Path.GetFileNameWithoutExtension(filePath);
+            }
+        }
+    }
+}
---
+++ src/Authentication.Abstractions/Interfaces/IContainsTelemetryErrorData.cs
@@ -0,0 +1,61 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions.Interfaces
+{
+    /// <summary>
+    /// Represent error info that should be sent to telemetry
+    /// </summary>
+    public interface IContainsTelemetryErrorData
+    {
+        /// <summary>
+        /// Error Kind: User, Serivce, Internal
+        /// </summary>
+        ErrorKind ErrorKind { get; }
+
+        /// <summary>
+        /// Desensitized error message
+        /// </summary>
+        string DesensitizedErrorMessage { get; }
+
+        /// <summary>
+        /// Line number where exception is thrown
+        /// </summary>
+        int? ErrorLineNumber { get; }
+
+        /// <summary>
+        /// File name in which exceptions is thrown
+        /// </summary>
+        string ErrorFileName { get; }
+
+    }
+
+    public class ErrorKind
+    {
+        public string Value { get; private set; }
+
+        private ErrorKind(string value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator string(ErrorKind error) => error.Value;
+
+        public static ErrorKind UserError = new ErrorKind("User");
+
+        public static ErrorKind ServiceError = new ErrorKind("Service");
+
+        public static ErrorKind InternalError = new ErrorKind("Internal");
+    }
+}
