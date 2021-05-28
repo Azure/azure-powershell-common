7f19987b195e96d6e54e75d9bd0a1b569f6836d8
Yabo Hu <yabhu@microsoft.com>
2021-05-28 17:13:43 +0800

    add constants.cs

---
+++ src/Share/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Microsoft.Azure.PowerShell.Share
+{
+    public class Constants
+    {
+        public static string ProfileDirectory = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".Azure");
+
+        public static string SurveyScheduleInfoFile = Path.Combine(ProfileDirectory, "AzureRmSurvey.json");
+
+        public const string AzurePSInterceptSurvey = "Azure_PS_Intercept_Survey";
+
+        public const string Predictor = "Az.Predictor";
+    }
+}
