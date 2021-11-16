bc1a33e7cfc94db1b75fdf6c556489ed7ecaf621
wyunchi-ms <yunwang@microsoft.com>
2021-11-16 17:07:36 +0800

    Move the methods to MicrosoftGraphClientExtensions

--- src/Graph.Rbac/MicrosoftGraph/Version1_0/MicrosoftGraphClient.cs
+++ src/Graph.Rbac/MicrosoftGraph/Version1_0/MicrosoftGraphClient.cs
@@ -7,16 +7,11 @@
 namespace Microsoft.Azure.Commands.Common.MSGraph.Version1_0
 {
     using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Applications;
-    using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Applications.Models;
     using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.DirectoryObjects;
     using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Groups;
-    using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Groups.Models;
     using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Users;
-    using Microsoft.Azure.Commands.Common.MSGraph.Version1_0.Users.Models;
-    using Microsoft.Azure.Commands.ResourceManager.Common.Paging;
     using Microsoft.Rest;
     using Microsoft.Rest.Azure;
-    using Microsoft.Rest.Azure.OData;
     using Microsoft.Rest.Serialization;
     using Newtonsoft.Json;
     using System.Collections;
@@ -24,7 +19,6 @@ namespace Microsoft.Azure.Commands.Common.MSGraph.Version1_0
     using System.Linq;
     using System.Net;
     using System.Net.Http;
-    using System.Web;
 
     /// <summary>
     /// The MSGraph Client.
@@ -344,173 +338,5 @@ namespace Microsoft.Azure.Commands.Common.MSGraph.Version1_0
             DeserializationSettings.Converters.Add(new CloudErrorJsonConverter());
         }
 
-        private string FormatFilterString<T>(ODataQuery<T> odataQuery)
-        {
-            return HttpUtility.UrlDecode(odataQuery.Filter);
-        }
-
-        public IEnumerable<MicrosoftGraphServicePrincipal> FilterServicePrincipals(MicrosoftObjectFilterOptions options)
-        {
-            List<MicrosoftGraphServicePrincipal> servicePrincipals = new List<MicrosoftGraphServicePrincipal>();
-            MicrosoftGraphServicePrincipal servicePrincipal = null;
-
-            if (!string.IsNullOrEmpty(options.Id))
-            {
-                try
-                {
-                    servicePrincipal = ServicePrincipals.GetServicePrincipal(options.Id);
-                }
-                catch {  /* The user does not exist, ignore the exception. */ }
-
-                if (servicePrincipal != null)
-                {
-                    servicePrincipals.Add(servicePrincipal);
-                }
-            }
-            else if (!string.IsNullOrEmpty(options.SPN))
-            {
-                try
-                {
-                    var odataQuery = new ODataQuery<MicrosoftGraphServicePrincipal>(s => s.ServicePrincipalNames.Contains(options.SPN));
-                    servicePrincipal = ServicePrincipals.ListServicePrincipal(filter: FormatFilterString(odataQuery)).Value.FirstOrDefault();
-                }
-                catch {  /* The user does not exist, ignore the exception. */ }
-
-                if (servicePrincipal != null)
-                {
-                    servicePrincipals.Add(servicePrincipal);
-                }
-            }
-            else
-            {
-                ODataQuery<MicrosoftGraphServicePrincipal> odataQuery = null;
-                if (!string.IsNullOrEmpty(options.SearchString) && options.SearchString.EndsWith("*"))
-                {
-                    options.SearchString = options.SearchString.TrimEnd('*');
-                    odataQuery = new ODataQuery<MicrosoftGraphServicePrincipal>(s => s.DisplayName.StartsWith(options.SearchString));
-                }
-                else
-                {
-                    odataQuery = new ODataQuery<MicrosoftGraphServicePrincipal>(s => s.DisplayName == options.SearchString);
-                }
-
-                return ServicePrincipals.ListServicePrincipal(filter: FormatFilterString(odataQuery)).Value;
-            }
-
-            return servicePrincipals;
-        }
-
-        public IEnumerable<MicrosoftGraphServicePrincipal> FilterServicePrincipals(ODataQuery<MicrosoftGraphServicePrincipal> odataQuery)
-        {
-            return ServicePrincipals.ListServicePrincipal(filter: FormatFilterString(odataQuery)).Value;
-        }
-
-        public IEnumerable<MicrosoftGraphUser> FilterUsers(MicrosoftObjectFilterOptions options)
-        {
-            if (!string.IsNullOrEmpty(options.Id))
-            {
-                try
-                {
-                    MicrosoftGraphUser user = Users.GetUser(options.Id);
-                    if (user != null)
-                    {
-                        return new List<MicrosoftGraphUser> { user };
-                    }
-                }
-                catch {  /* The group does not exist, ignore the exception */ }
-            }
-            else if (!string.IsNullOrEmpty(options.UPN) || !string.IsNullOrEmpty(options.Mail))
-            {
-                IList<MicrosoftGraphUser> result = null;
-                try
-                {
-                    string upnOrMail = options.UPN ?? options.Mail;
-                    var odataQuery = new ODataQuery<MicrosoftGraphUser>();
-                    if (!string.IsNullOrEmpty(options.UPN))
-                    {
-                        odataQuery.SetFilter(u => u.UserPrincipalName == upnOrMail);
-                    }
-                    else
-                    {
-                        odataQuery.SetFilter(u => u.Mail == upnOrMail);
-                    }
-                    result = Users.ListUser(filter: FormatFilterString(odataQuery)).Value;
-                }
-                catch {  /* The user does not exist, ignore the exception. */ }
-
-                if (result != null)
-                {
-                    return result.Select(u => u);
-                }
-            }
-            else
-            {
-                ODataQuery<MicrosoftGraphUser> odataQuery = null;
-                if (!string.IsNullOrEmpty(options.SearchString) && options.SearchString.EndsWith("*"))
-                {
-                    options.SearchString = options.SearchString.TrimEnd('*');
-                    odataQuery = new ODataQuery<MicrosoftGraphUser>(u => u.DisplayName.StartsWith(options.SearchString));
-                }
-                else
-                {
-                    odataQuery = new ODataQuery<MicrosoftGraphUser>(u => u.DisplayName == options.SearchString);
-                }
-
-                return Users.ListUser(filter: FormatFilterString(odataQuery)).Value;
-            }
-
-            return new List<MicrosoftGraphUser>();
-        }
-
-        public IEnumerable<MicrosoftGraphUser> FilterUsers(ODataQuery<MicrosoftGraphUser> odataQuery)
-        {
-            return Users.ListUser(filter: FormatFilterString(odataQuery)).Value;
-        }
-
-        public IEnumerable<MicrosoftGraphGroup> FilterGroups(MicrosoftObjectFilterOptions options)
-        {
-            if (!string.IsNullOrEmpty(options.Id))
-            {
-                try
-                {
-                    // use GetObjectsByObjectId to handle Redirects in the CSP scenario
-                    MicrosoftGraphGroup group = Groups.GetGroup(options.Id);
-                    if (group != null)
-                    {
-                        return new List<MicrosoftGraphGroup> { group };
-                    }
-                }
-                catch {  /* The group does not exist, ignore the exception */ }
-            }
-            else
-            {
-                ODataQuery<MicrosoftGraphGroup> odataQuery = null;
-                if (options.Mail != null)
-                {
-                    odataQuery = new ODataQuery<MicrosoftGraphGroup>(g => g.Mail == options.Mail);
-                }
-                else
-                {
-                    if (!string.IsNullOrEmpty(options.SearchString) && options.SearchString.EndsWith("*"))
-                    {
-                        options.SearchString = options.SearchString.TrimEnd('*');
-                        odataQuery = new ODataQuery<MicrosoftGraphGroup>(g => g.DisplayName.StartsWith(options.SearchString));
-                    }
-                    else
-                    {
-                        odataQuery = new ODataQuery<MicrosoftGraphGroup>(g => g.DisplayName == options.SearchString);
-                    }
-                }
-
-                return Groups.ListGroup(filter: FormatFilterString(odataQuery)).Value;
-            }
-
-            return new List<MicrosoftGraphGroup>();
-        }
-
-        public IEnumerable<MicrosoftGraphGroup> FilterGroups(ODataQuery<MicrosoftGraphGroup> odataQuery)
-        {
-            return Groups.ListGroup(filter: FormatFilterString(odataQuery)).Value;
-        }
     }
 }
