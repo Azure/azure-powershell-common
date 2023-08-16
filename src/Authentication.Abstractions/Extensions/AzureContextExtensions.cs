c2e035bf28ebe29d5e9a86fd08d7be95b4fc7f19
Lei Jin <leijin@microsoft.com>
2023-08-16 15:29:32 +0800

    Address review comments

--- src/Authentication.Abstractions/Extensions/AzureContextExtensions.cs
+++ src/Authentication.Abstractions/Extensions/AzureContextExtensions.cs
@@ -171,6 +171,8 @@ namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
             deepCopy.Tenant.CopyFrom(context.Tenant);
             deepCopy.Subscription.CopyFrom(context.Subscription);
             deepCopy.Environment.CopyFrom(context.Environment);
+
+            //TODO：Whenever to introduce more properties that needs to be copied here, extract the logic to a new method.
             if (context.Tenant is AzureTenant azureTenant)
             {
                 ((AzureTenant)deepCopy.Tenant).Directory = azureTenant.Directory;
