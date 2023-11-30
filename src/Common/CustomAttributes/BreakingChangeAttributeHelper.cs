ceb2c410dc5e83a10859db169882eb692a18e891
Yabo Hu <yabhu@microsoft.com>
2023-11-30 12:24:01 +0800

    support breaking change attribute for dynamic parameters (#407)
    * get breaking change attribute for all parameters instead of static parameters only, in this way breaking change attribute for dynamic parameters are able to be displayed
    
    * delete unused method

--- src/Common/CustomAttributes/BreakingChangeAttributeHelper.cs
+++ src/Common/CustomAttributes/BreakingChangeAttributeHelper.cs
@@ -20,6 +20,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Microsoft.WindowsAzure.Commands.Common.CustomAttributes
@@ -154,9 +155,9 @@ namespace Microsoft.WindowsAzure.Commands.Common.CustomAttributes
                 attributeList.AddRange(f.GetCustomAttributes(typeof(GenericBreakingChangeWithVersionAttribute), false).Cast<GenericBreakingChangeWithVersionAttribute>());
             }
 
-            foreach (PropertyInfo p in type.GetRuntimeProperties())
+            foreach (ParameterMetadata param in invocationInfo.MyCommand.Parameters.Values)
             {
-                    attributeList.AddRange(p.GetCustomAttributes(typeof(GenericBreakingChangeWithVersionAttribute), false).Cast<GenericBreakingChangeWithVersionAttribute>());
+                attributeList.AddRange(param.Attributes.Where(a => a is GenericBreakingChangeWithVersionAttribute).Select(a => a as GenericBreakingChangeWithVersionAttribute));
             }
 
             return invocationInfo == null ? attributeList : attributeList.Where(e => e.IsApplicableToInvocation(invocationInfo));
