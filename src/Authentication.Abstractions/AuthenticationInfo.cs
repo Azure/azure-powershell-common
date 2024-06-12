2a3b1b6708d82403b7abc52d6b6f3e3b273d7dba
Lei jin <leijin@microsoft.com>
2024-06-12 13:46:48 +0800

    Add Interface to Send Auth Info to Telemetry

---
+++ src/Authentication.Abstractions/AuthenticationInfo.cs
@@ -0,0 +1,48 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Concurrent;
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Azure.Commands.Common.Authentication.Abstractions
+{
+    public class AuthenticationInfo : IAuthenticationInfo
+    {
+        /// <summary>
+        /// Class name of the TokenCredential, stands for the authentication method
+        /// </summary>
+        public string TokenCredentialName { get; set; }
+
+        /// <summary>
+        /// Authority Uri to do the authentiation
+        /// </summary>
+        public string AuthorityUri { get; set; }
+
+        /// <summary>
+        /// Authentication process succeed or not.
+        /// </summary>
+        public bool AuthenticationSuccess { get; set; } = false;
+
+        /// <summary>
+        /// Additional properties for AuthenticationInfo
+        /// </summary>
+        public IDictionary<string, string> ExtendedProperties { get; } = new ConcurrentDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Key to show whether token cache is enabled or not.
+        /// </summary>
+        public const string TokenCacheEnabled = "TokenCacheEnabled";
+    }
+}
